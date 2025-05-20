7fad86ec6d2e94113cda12f5e6efd0589a39f89e
barbourians <barbourians@gmail.com>
2025-05-20 11:33:55 +0100

    add git checks

--- src/activate.sh
+++ src/activate.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 # Check untracked
-rc=$(git status | grep Untracked | wc -l)
+rc=$(git status | grep "Untracked" | wc -l)
 if [ ${rc} -ne 0 ]; then
-  echo "WARNING: There are untracked files. Please commit"
+  git status
+  echo -e "\nWARNING: There are untracked files. Please add!"
   exit 1
+fi
+
+# Check new but not committed files
+rc=$(git status | grep "Changes to be" | wc -l)
+if [ ${rc} -ne 0 ]; then
+  git status
+  echo -e "\nWARNING: There are new files not commited. Please commit!"
+  exit 2
+fi
+
+# Check new but not committed files
+rc=$(git status | grep "Changes not staged" | wc -l)
+if [ ${rc} -ne 0 ]; then
+  git status
+  echo -e "\nWARNING: There are modified files. Please commit!"
+  exit 3
 fi
